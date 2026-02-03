8b4030d4db544bc4addd57296594b9ee1937e5b7
Alson Basnet <basnetalson121@gmail.com>
2026-02-03 22:27:29 +0545

    feat: match case example

---
+++ loops/2.py
@@ -0,0 +1,11 @@
+#match case
+a=22
+match a:
+    case 22:
+        print("The value is 22")
+    case 33:
+        print("The value is 33")
+    case 44:
+        print("The value is 44")
+    case _:
+        print("It doesnt match case's")
