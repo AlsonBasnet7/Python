4d2d1c858b6db5fd80260476aaa630df11377d2d
Alson Basnet <basnetalson121@gmail.com>
2026-02-16 08:33:03 +0545

    Feat:FString

---
+++ string/02_fstring.py
@@ -0,0 +1,9 @@
+name="Alson"
+caste="Basnet"
+print(f"My name is {name} {caste}.")
+text = "Python"
+print(f"{text:>10}")  # Right align
+print(f"{text:<10}")  # Left align
+print(f"{text:^10}")  # Center align
+print(ord('A'))  # Output: 65 #This is giving me the unicode.
+print(chr(65))   # Output: 'A'
---
+++ string/readme.txt
@@ -0,0 +1,1 @@
+This is where i have done the string part of the Python.
