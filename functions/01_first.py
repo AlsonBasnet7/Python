df92911c51141464baa333bcfc48ffbac74e231d
Alson Basnet <basnetalson121@gmail.com>
2026-02-16 10:50:50 +0545

    feat:methods

---
+++ functions/01_first.py
@@ -0,0 +1,10 @@
+# we use def keyword while declaring the function.
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Alice"))  # Output: Hello, Alice!
+
+
+def greeting(name):
+    return f"Hello,{name}!"
+print(greet("Alson"))
