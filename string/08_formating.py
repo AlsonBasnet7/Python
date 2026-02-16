074177d84120af00c74bd1551733996cff49f57a
Alson Basnet <basnetalson121@gmail.com>
2026-02-16 10:21:40 +0545

    feat:formating

---
+++ string/08_formating.py
@@ -0,0 +1,16 @@
+# 4. String Formatting and f-Strings
+# Using format(), create a sentence:
+# "My name is John and I am 25 years old."
+# by passing "John" and 25 as variables.
+
+# Do the same using f-strings.
+name="John"
+age="25"
+print(f"My name is {name} adn I am {25} years old.")
+
+
+name = "John"
+age = 25
+
+# Using format()
+print("My name is {} and I am {} years old.".format(name, age))
