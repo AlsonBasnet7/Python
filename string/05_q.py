862837ddd40e92d672ae00a3962883f85fc0d116
Alson Basnet <basnetalson121@gmail.com>
2026-02-16 08:57:08 +0545

    feat:slicing and indexing

---
+++ string/05_q.py
@@ -0,0 +1,8 @@
+# Given text = "Python Programming", do the following:
+# Print the first 6 characters
+# Print the last 6 characters
+# Print every second character from the string
+text="Python Progamming"
+print("The first 6 characters",text[0:6])
+print("The last 6 characters ",text[-6:])
+print("Every second character from the string",text[::2])
