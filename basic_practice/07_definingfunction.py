cfdd99eb48d131988dd9281e00ea25dd570f1714
Alson Basnet <alsonbasnet@Alsons-MacBook-Air.local>
2026-02-17 23:01:11 +0545

    Some functions related questions

---
+++ basic_practice/07_definingfunction.py
@@ -0,0 +1,10 @@
+# Write a function greet() that prints "Hello, Python Learner!" when called.
+# Write a function square(num) that returns the square of a given number. Test it with different numbers.
+
+def greet():
+    return print("Hello, I am learning Python")
+greet()
+
+def sqaure(n):
+    return n^2
+print("The square of the number is",sqaure(4))
---
+++ basic_practice/08_definingfunction.py
@@ -0,0 +1,4 @@
+# Write a function square(num) that returns the square of a given number. Test it with different numbers.
+def square(n):
+    return n**2
+print("The square of the number is ", square(3))
