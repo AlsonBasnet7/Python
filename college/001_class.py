9009810a254ed8936a28a31adeb0b6f722acc9f3
Alson Basnet <basnetalson121@gmail.com>
2026-03-18 14:42:43 +0545

    feat:sorting

---
+++ college/001_class.py
@@ -0,0 +1,5 @@
+#Write a program that prints out all the elements of a. lsit which are divisible by 2 and 3. use the following list.
+a=[2,3,6,8,912,13,18,24,22,22,12]
+for i in a:
+    if i%2==0 and i%3==0:
+        print(i)
